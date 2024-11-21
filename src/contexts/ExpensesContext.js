d5bef410e06acbb077cdf3dea467d4972c00b004
Nikhilhaspe <nikhilhaspe5220@gmail.com>
2024-11-21 22:42:18 +0530

    expense context setup

---
+++ src/contexts/ExpensesContext.js
@@ -0,0 +1,38 @@
+// library imports
+import { useReducer } from "react";
+import { createContext, useContext } from "react";
+
+// context
+const ExpensesContext = createContext();
+// reducer
+const INITIAL_STATE = {
+  expenses: [],
+};
+function reducer(state, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+function ExpensesContextProvider(props) {
+  // props
+  const { children } = props;
+  // state
+  const [{ expenses }, dispatch] = useReducer(reducer, INITIAL_STATE);
+
+  console.log(expenses, dispatch);
+
+  return <ExpensesContext.Provider>{children}</ExpensesContext.Provider>;
+}
+
+export function useExpenses() {
+  const context = useContext(ExpensesContext);
+
+  if (!context)
+    throw new Error("EXPENSES CONTEXT WAS USED OUTSIDE OF IT'S PROVIDER!");
+
+  return context;
+}
+
+export default ExpensesContextProvider;
